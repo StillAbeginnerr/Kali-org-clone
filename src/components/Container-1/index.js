182a2fa164e8873811bd1c88a5bbfbe0ba36f365
shivam agarwal <shivamagarwaloff@gmail.com>
2022-04-02 05:18:47 +0530

    container-1

--- src/components/Container-1/index.js
+++ src/components/Container-1/index.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import "./index.css"
 
 const ContainerOne = () => {
   return (
-    <div className='container-top-half'>
-            <h1 id="heading">The Industry Standard</h1>
+    <div className='container-one'>
+    <section className='first'>
+    <div>
+      <div>
+        <h1 className='first-text'>The Industry Standard</h1>
+        </div>
+        <div className='second-text'>
+        Kali Linux is not about its tools, nor the operating <br></br>system. Kali Linux is a <b>platform</b>.
+        </div>
+          <div className='third-text'>
+         Make Your Job Easier 
+          </div>
+          <div className='fourth-text'>
+         Kali Everywhere
+          </div>
+          <div className='five-text'>
+         Customization
+          </div>
+
+        </div>
+    </section>
+            
     </div>
   )
 }
